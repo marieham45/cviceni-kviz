2da696fa077b6e030eac43614eac20902539c9aa
marieham45 <marie.hamsikova@centrum.cz>
2024-10-21 19:35:13 +0200

    reseni kviz

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+/*
+Napište JavaScriptový program, který vybere element s třídou question a nastaví jeho obsah na nějakou kvízovou otázku, například:
 
+Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.
+
+Použijte funkci prompt a zeptejte se uživatele na správnou odpověď. Vyberte element s třídou answer__text a nastavte obsah elementu na odpověď od uživatele. Vyberte element s třidou answer. Pokud je odpověď příjmení Bell, přidejte k tomuto elementu třídu answer--correct. V opačném případě přidejte třídu answer--wrong.
+*/
+
+const question = document.querySelector(".question");
+
+question.textContent =
+  "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.";
+
+const answerPrompt = prompt("Odpověď:").toUpperCase(); // odpověď dáváme do kapitálek, abychom mohli sbírat všechny formy odpovědi - bell, Bell i BeLL apod. :-)
+
+const answer = document.querySelector(".answer__text");
+answer.textContent = answerPrompt;
+
+if (answerPrompt === "BELL") {
+  answer.classList.add("answer--correct");
+} else {
+  answer.classList.add("answer--wrong");
+}
